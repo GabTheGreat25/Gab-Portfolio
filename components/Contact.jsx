82dee4f47544d1ff9cb42dda523290878a744a3d
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-06 01:55:50 +0800

    Add url link to send message

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -125,7 +125,11 @@ const Contact = () => {
           {/* right */}
           <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
             <div className="p-4">
-              <form action="" method="POST" encType="multipart/form-data">
+              <form
+                action="https://getform.io/f/66bad9b1-3e7b-445e-a0a5-add0122bc6a3"
+                method="POST"
+                encType="multipart/form-data"
+              >
                 <div className="grid md:grid-cols-2 gap-4 w-full py-2">
                   <div className="flex flex-col">
                     <label className="text-lg py-2">Name</label>
