236d3e125916ba3dee0dc0da6b7ca7cbbb36d864
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-06 11:37:39 +0800

    Add smaller custom screen in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: { min: "375px" },
+        xss: { min: "320px" },
+      },
+    },
   },
   plugins: [],
 };
