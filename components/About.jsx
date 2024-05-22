bf4fdec4b96187514bbebad928e69df782ba6ede
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-05-22 16:24:38 +0800

    Add new projects

--- components/About.jsx
+++ components/About.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import Image from "next/image";
 import AboutImg from "../public/assets/about-me/gab-pic.jpg";
+import MonaCatImg from "../public/assets/about-me/MonaCat.gif";
 
 const About = () => {
   return (
-    <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
+    <div id="about" className="flex items-center w-full p-2 py-16 md:h-screen">
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
         <div className="col-span-2">
           <p className="font-semibold text-center text-5xl pb-4 tracking-widest text-[#5651e5]">
-            About Myself
+            About Myself{" "}
+            <span className="relative top-[10px]">
+              <Image width="75" height="60" src={MonaCatImg} alt="MonaCat" />
+            </span>
           </p>
-          <p className="py-2 text-gray-600 italic font-medium text-justify">
+          <p className="py-2 italic font-medium text-justify text-gray-600">
             As a passionate learner in the field of web development, I have been
             exploring various aspects of this exciting industry. My primary
             focus has been on front-end development, where I have honed my
@@ -19,7 +23,7 @@ const About = () => {
             development to ensure that my projects have a solid foundation and
             optimal performance.
           </p>
-          <p className="py-2 text-gray-600 italic font-medium text-justify">
+          <p className="py-2 italic font-medium text-justify text-gray-600">
             In my free time, I have been delving into the world of web3, the
             decentralized web, to gain a better understanding of how this
             emerging technology can revolutionize the way we interact with the
@@ -27,15 +31,15 @@ const About = () => {
             developed a strong understanding of blockchain technology and its
             potential applications in web development.
           </p>
-          <p className="py-2 text-gray-600 italic font-medium text-justify">
+          <p className="py-2 italic font-medium text-justify text-gray-600">
             As I continue on my learning journey, I am eager to explore new
             technologies and push the boundaries of what is possible in web
             development. I am excited to apply my knowledge and skills to create
             innovative solutions and deliver exceptional user experiences.
           </p>
         </div>
-        <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
-          <Image src={AboutImg} className="rounded-xl" alt="/" />
+        <div className="flex items-center justify-center w-full h-auto p-4 m-auto duration-300 ease-in shadow-xl shadow-gray-400 rounded-xl hover:scale-105">
+          <Image src={AboutImg} className="rounded-xl" alt="Gab" />
         </div>
       </div>
     </div>
--- pages/projects.jsx
+++ pages/projects.jsx
@@ -4,6 +4,13 @@ import Image from "next/image";
 import VloggersTvImg from "../public/assets/projects/VloggersTv.png";
 import CameraRentalImg from "../public/assets/projects/CameraRental.png";
 import LhanleeSalonImg from "../public/assets/projects/LhanleeSalon.png";
+import BauchaImg from "../public/assets/projects/Baucha.png";
+import LaravelPetCareImg from "../public/assets/projects/LaravelPetCare.png";
+import PhpPetClinicImg from "../public/assets/projects/PhpPetClinic.png";
+import VloggersImg from "../public/assets/projects/Vloggers.png";
+import MayaCLIImg from "../public/assets/projects/MayaCLI.png";
+import PromdiImg from "../public/assets/projects/Promdi.png";
+import YahuImg from "../public/assets/projects/Yahu.png";
 
 const projects = () => {
   return (
@@ -20,37 +27,36 @@ const projects = () => {
         id="projects"
         className="xss:px-10 lg:px-0 pb-12 max-w-[1400px] mx-auto pt-[120px]"
       >
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 md:grid-cols-2 lg:grid-cols-3">
           <div className="overflow-hidden bg-white rounded-lg shadow-lg">
             <a
-              href="https://gabthegreat25.github.io/Final-Project-ITOS322-T/"
-              className="text-black"
+              href="https://github.com/GabTheGreat25/php_pet_clinic"
+              className="text-black "
             >
               <Image
                 height="100%"
                 width="100%"
                 layout="responsive"
-                src={VloggersTvImg}
-                alt="VloggersTv"
+                src={LhanleeSalonImg}
+                alt="LhanleeSalon"
               />
             </a>
             <div className="p-4">
               <a
-                href="https://gabthegreat25.github.io/Final-Project-ITOS322-T/"
+                href="https://www.lhanlee-salon.com/"
                 className="text-black hover:text-[#5651e5]"
               >
-                <h2 className="mb-2 text-2xl font-bold">VloggersTv</h2>
+                <h2 className="mb-2 text-2xl font-bold">Lhanlee Salon</h2>
               </a>
 
               <p className="text-gray-800">
-                This project was created using Tailwind, SCSS, HTML, and JS.
-                Tailwind provided a customizable CSS framework, while SCSS was
-                used for maintainable and organized CSS. HTML structured the web
-                pages, and JS added interactivity and dynamic behavior. These
-                tools enabled the creation of a modern, responsive, and
-                functional web application. The application was deployed using
-                GitHub Pages, which hosts static websites directly from a GitHub
-                repository, allowing for easy and free deployment.
+                This project is built using the MERN stack (MongoDB, Express,
+                React + Vite + Million.js, Node.js), enhanced with Redux Toolkit
+                for state management and RTK Query for efficient data fetching.
+                It features Tailwind CSS with DaisyUI for the User Interface,
+                PayMaya (Maya) for payments, Twilio for notifications, and React
+                Native for the mobile app. The backend is deployed using Render,
+                and the frontend is deployed using Vercel.
               </p>
             </div>
           </div>
@@ -86,31 +92,264 @@ const projects = () => {
             </div>
           </div>
           <div className="overflow-hidden bg-white rounded-lg shadow-lg">
-            <a href="https://www.lhanlee-salon.com/" className="text-black ">
+            <a
+              href="https://gabthegreat25.github.io/Final-Project-ITOS322-T/"
+              className="text-black"
+            >
               <Image
                 height="100%"
                 width="100%"
                 layout="responsive"
-                src={LhanleeSalonImg}
-                alt="LhanleeSalon"
+                src={VloggersTvImg}
+                alt="VloggersTv"
               />
             </a>
             <div className="p-4">
               <a
-                href="https://camera-rental-fe.vercel.app/"
+                href="https://gabthegreat25.github.io/Final-Project-ITOS322-T/"
                 className="text-black hover:text-[#5651e5]"
               >
-                <h2 className="mb-2 text-2xl font-bold">Lhanlee Salon</h2>
+                <h2 className="mb-2 text-2xl font-bold">
+                  Final-Project-ITOS322-T
+                </h2>
               </a>
 
               <p className="text-gray-800">
-                This project is built using the MERN stack (MongoDB, Express,
-                React + Vite + Million.js, Node.js), enhanced with Redux Toolkit
-                for state management and RTK Query for efficient data fetching.
-                It features Tailwind CSS with DaisyUI for the User Interface,
-                PayMaya (Maya) for payments, Twilio for notifications, and React
-                Native for the mobile app. The backend is deployed using Render,
-                and the frontend is deployed using Vercel.
+                This project was created using Tailwind, SCSS, HTML, and JS.
+                Tailwind provided a customizable CSS framework, while SCSS was
+                used for maintainable and organized CSS. HTML structured the web
+                pages, and JS added interactivity and dynamic behavior. These
+                tools enabled the creation of a modern, responsive, and
+                functional web application. The application was deployed using
+                GitHub Pages.
+              </p>
+            </div>
+          </div>
+
+          <div className="overflow-hidden bg-white rounded-lg shadow-lg">
+            <a
+              href="https://www.figma.com/design/GuV6hEJOj63s1QcWl8GWZr/crypto-8?node-id=0-1&t=RBekDImdGqaM1r6P-0"
+              className="text-black "
+            >
+              <Image
+                height="100%"
+                width="100%"
+                layout="responsive"
+                src={BauchaImg}
+                alt="Baucha"
+              />
+            </a>
+            <div className="p-4">
+              <a
+                href="https://www.figma.com/design/GuV6hEJOj63s1QcWl8GWZr/crypto-8?node-id=0-1&t=RBekDImdGqaM1r6P-0"
+                className="text-black hover:text-[#5651e5]"
+              >
+                <h2 className="mb-2 text-2xl font-bold">
+                  BAUCHA Crypto Voucher
+                </h2>
+              </a>
+
+              <p className="text-gray-800">
+                This project was my first ever hackathon where I contributed to
+                the frontend using React and helped with Figma for design. This
+                experience gave me a hands-on opportunity to apply my skills in
+                a collaborative environment and work with a dynamic team to
+                build a functional web application.
+              </p>
+            </div>
+          </div>
+          <div className="overflow-hidden bg-white rounded-lg shadow-lg">
+            <a
+              href="https://github.com/GabTheGreat25/Mendoza-Medalla-Repository"
+              className="text-black"
+            >
+              <Image
+                height="100%"
+                width="100%"
+                layout="responsive"
+                src={LaravelPetCareImg}
+                alt="LaravelPetCare"
+              />
+            </a>
+            <div className="p-4">
+              <a
+                href="https://github.com/GabTheGreat25/Mendoza-Medalla-Repository"
+                className="text-black hover:text-[#5651e5]"
+              >
+                <h2 className="mb-2 text-2xl font-bold">Pet Care</h2>
+              </a>
+
+              <p className="text-gray-800">
+                This project was built using Laravel and Tailwind. Laravel
+                provided a powerful and elegant PHP framework for backend
+                development, while Tailwind offered a customizable CSS framework
+                for styling. HTML structured the web pages, and JavaScript added
+                interactivity and dynamic behavior. These tools enabled the
+                creation of a modern and responsive web application.
+              </p>
+            </div>
+          </div>
+          <div className="overflow-hidden bg-white rounded-lg shadow-lg">
+            <a
+              href="https://github.com/GabTheGreat25/php_pet_clinic"
+              className="text-black "
+            >
+              <Image
+                height="100%"
+                width="100%"
+                layout="responsive"
+                src={PhpPetClinicImg}
+                alt="PhpPetClinic"
+              />
+            </a>
+            <div className="p-4">
+              <a
+                href="https://github.com/GabTheGreat25/php_pet_clinic"
+                className="text-black hover:text-[#5651e5]"
+              >
+                <h2 className="mb-2 text-2xl font-bold">Pet Clinic</h2>
+              </a>
+
+              <p className="text-gray-800">
+                This project was built using PHP, HTML, CSS, and JavaScript. PHP
+                provided a powerful and elegant framework for backend
+                development, while CSS offered a customizable framework for
+                styling. HTML structured the web pages, and JavaScript added
+                interactivity and dynamic behavior. Together, these technologies
+                ensured the creation of a modern and responsive web application.
+              </p>
+            </div>
+          </div>
+
+          <div className="overflow-hidden bg-white rounded-lg shadow-lg">
+            <a
+              href="https://github.com/GabTheGreat25/VloggersTV"
+              className="text-black "
+            >
+              <Image
+                height="100%"
+                width="100%"
+                layout="responsive"
+                src={VloggersImg}
+                alt="Vloggers"
+              />
+            </a>
+            <div className="p-4">
+              <a
+                href="https://github.com/GabTheGreat25/VloggersTV"
+                className="text-black hover:text-[#5651e5]"
+              >
+                <h2 className="mb-2 text-2xl font-bold">VloggersTV</h2>
+              </a>
+
+              <p className="text-gray-800">
+                This project was created using Angular for the frontend, along
+                with SCSS and Tailwind for styling. I contributed significantly
+                to the frontend development in Angular, ensuring the web
+                application was dynamic and responsive. Additionally, I played a
+                key role in the design phase, using Figma to create a cohesive
+                and visually appealing interface. This experience provided me
+                with the opportunity to apply my skills in a collaborative
+                environment, working with a dynamic team to build a functional
+                and modern web application.
+              </p>
+            </div>
+          </div>
+          <div className="overflow-hidden bg-white rounded-lg shadow-lg">
+            <a
+              href="https://github.com/GabTheGreat25/cli"
+              className="text-black"
+            >
+              <Image
+                height="100%"
+                width="100%"
+                layout="responsive"
+                src={MayaCLIImg}
+                alt="MayaCLI"
+              />
+            </a>
+            <div className="p-4">
+              <a
+                href="https://github.com/GabTheGreat25/cli"
+                className="text-black hover:text-[#5651e5]"
+              >
+                <h2 className="mb-2 text-2xl font-bold">MayaCLI Web</h2>
+              </a>
+
+              <p className="text-gray-800">
+                This project utilized Angular for the frontend, complemented by
+                SCSS and Tailwind for styling. I made substantial contributions
+                to the frontend development, focusing on ensuring the web
+                application's dynamism and responsiveness. Additionally, I
+                played a pivotal role in the design phase, utilizing Figma to
+                craft a cohesive and visually appealing interface. This
+                experience afforded me the chance to apply my skills in a
+                collaborative setting, collaborating with a dynamic team to
+                construct a robust and cutting-edge web application.
+              </p>
+            </div>
+          </div>
+          <div className="overflow-hidden bg-white rounded-lg shadow-lg">
+            <a
+              href="https://github.com/GabTheGreat25/yahu-rider-web"
+              className="text-black "
+            >
+              <Image
+                height="100%"
+                width="100%"
+                layout="responsive"
+                src={YahuImg}
+                alt="Yahu"
+              />
+            </a>
+            <div className="p-4">
+              <a
+                href="https://github.com/GabTheGreat25/yahu-rider-web"
+                className="text-black hover:text-[#5651e5]"
+              >
+                <h2 className="mb-2 text-2xl font-bold">Yahu</h2>
+              </a>
+
+              <p className="text-gray-800">
+                This project was developed using Angular for the frontend, with
+                SCSS and Tailwind for styling. I played a major role in the
+                Angular frontend development, ensuring the web application was
+                dynamic and responsive. I also contributed significantly to the
+                design phase, using Figma to create a cohesive and visually
+                appealing interface. This experience enabled me to utilize my
+                skills in a collaborative setting, working with a dedicated team
+                to develop an effective and up-to-date web application.
+              </p>
+            </div>
+          </div>
+
+          <div className="overflow-hidden bg-white rounded-lg shadow-lg">
+            <a
+              href="https://github.com/GabTheGreat25/prodifarm-web"
+              className="text-black "
+            >
+              <Image
+                height="100%"
+                width="100%"
+                layout="responsive"
+                src={PromdiImg}
+                alt="Promdi"
+              />
+            </a>
+            <div className="p-4">
+              <a
+                href="https://github.com/GabTheGreat25/prodifarm-web"
+                className="text-black hover:text-[#5651e5]"
+              >
+                <h2 className="mb-2 text-2xl font-bold">PromdiFarm</h2>
+              </a>
+
+              <p className="text-gray-800">
+                This project was built using Angular and SCSS. Angular provided
+                a powerful framework for frontend development, while SCSS
+                offered a customizable framework for styling. I contributed by
+                adding design elements and implementing logic, ensuring the
+                creation of a responsive and dynamic web application.
               </p>
             </div>
           </div>
